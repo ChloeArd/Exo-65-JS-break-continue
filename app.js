5b70ef58df7b6c5716532a341f4e7c324ab61620
ChloeArd <chloe.ardoise@gmail.com>
2020-12-01 15:09:51 +0100

    Exercice 65

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ let elements = document.getElementsByClassName('paragraphe');
 
 for ( i = 0; i < elements.length; i++) {
     let paragraphe = elements[i];
-    paragraphe.innerHTML = "la variable i vaut: " + i;
 
     if ((i + 1) % 2) {
         paragraphe.innerHTML = " ";
         continue;
     }
 
-    if ( i === (elements.length - 1) ) {
+    else if ( i === (elements.length - 1)) {
         break;
+    }
+
+    else {
+        paragraphe.innerHTML = "la variable i vaut: " + i;
     }
 }
